81f86879b7daea3aeb22a574ad4a880bc2d67f69
Jan-Erik Rediger <janerik@fnordig.de>
2015-11-21 22:46:06 +0100

    Avoid raw pointer where possible

--- src/redis.rs
+++ src/redis.rs
@@ -3,10 +3,10 @@ use libc::c_void;
 pub type Client = *const c_void;
 
 extern {
-    fn addReplyString(client: *const c_void, s: *const u8, len: usize);
+    fn addReplyString(client: Client, s: *const u8, len: usize);
 }
 
-pub fn add_reply(client: *const c_void, reply: &str) {
+pub fn add_reply(client: Client, reply: &str) {
     unsafe {
         let reply = format!("{}\r\n\0", reply);
         let bytes = reply.as_bytes();
