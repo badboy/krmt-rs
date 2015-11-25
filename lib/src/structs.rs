e342392ce78481430dd5a2846af3d513a0c90d35
Jan-Erik Rediger <janerik@fnordig.de>
2015-11-25 20:13:56 +0100

    Replicate redisClient structure

--- lib/src/structs.rs
+++ lib/src/structs.rs
@@ -1,4 +1,4 @@
-use libc::{c_void, c_int, c_longlong};
+use libc::{c_void, c_int, c_long, c_longlong, c_ulong, time_t, c_char};
 use std::ptr;
 
 #[repr(C)]
@@ -27,6 +27,55 @@ pub struct redisModule {
     pub load: Option<extern "C" fn() -> *const c_void>,
     pub cleanup: Option<extern "C" fn()>,
 }
+
+#[repr(C)]
+#[allow(dead_code)]
+pub struct redisClient {
+    id: u64,
+    fd: c_int,
+    db: *const c_void,
+    dictid: c_int,
+    name: *const c_void,
+    querybuf: *const c_void,
+    querybuf_peak: usize,
+    argc: c_int,
+    argv: *const *const c_void,
+    cmd: *const c_void,
+    lastcmd: *const c_void,
+    reqtype: c_int,
+    multibulklen: c_int,
+    bulklen: c_long,
+    reply: *const c_void,
+    reply_bytes: c_ulong,
+    sentlen: c_int,
+    ctime: time_t,
+    lastinteraction: time_t,
+    obuf_soft_limit_reached_time: time_t,
+    flags: c_int,
+    authenticated: c_int,
+    replstate: c_int,
+    repl_put_online_on_ack: c_int,
+    repldbfd: c_int,
+    repldboff: u64,
+    repldbsize: u64,
+    replpreamble: *const c_void,
+    reploff: c_longlong,
+    repl_ack_off: c_longlong,
+    repl_ack_time: c_longlong,
+    replrunid: [c_char; 41],
+    slave_listening_port: c_int,
+    mstate: [u8; 24],
+    btype: c_int,
+    bpop: [u8; 40],
+    woff: c_longlong,
+    watched_keys: *const c_void,
+    pubsub_channels: *const c_void,
+    pubsub_patterns: *const c_void,
+    peerid: *const c_void,
+    bufpos: c_int,
+    buf: [u8; (16*1024)]
+}
+
 
 // It's not
 unsafe impl Sync for redisCommand {}
